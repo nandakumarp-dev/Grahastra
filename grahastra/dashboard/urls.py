b574e77f97fce6799cf04173233ee4a8b4198c74
nandakumarp-dev <nandakumarp.dev@gmail.com>
2025-06-05 14:00:58 +0000

    ceated all pages that are linked to dashboard

--- grahastra/dashboard/urls.py
+++ grahastra/dashboard/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('dashboard/',views.DashboardView.as_view(), name='dashboard_page'),
     path('mychart/',views.MyChartView.as_view(), name='mychart_page'),
     path('contact/',views.ContactView.as_view(), name='contact_page'),
+    path('ask_astrology/',views.AskAstrologyView.as_view(), name='ask_astrology_page'),
+    path('profile/',views.ProfileView.as_view(), name='profile_page'),
+    path('yogas/',views.YogasView.as_view(), name='yogas_page'),
 
 ]
--- grahastra/dashboard/views.py
+++ grahastra/dashboard/views.py
@@ -22,3 +22,22 @@ class ContactView(View):
     def get(self,request,*args,**kwargs):
 
          return render(request,'dashboard/contact_page.html')
+    
+
+class AskAstrologyView(View):
+
+    def get(self,request,*args,**kwargs):
+
+         return render(request,'dashboard/ask_astrology_page.html')
+    
+class ProfileView(View):
+
+    def get(self,request,*args,**kwargs):
+
+         return render(request,'dashboard/profile_page.html')
+    
+class YogasView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,'dashboard/yogas_page.html')
