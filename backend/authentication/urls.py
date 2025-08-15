49c84658b3bb61da5983aa280f315d5c2c8e90c2
nandakumarp-dev <nandakumarp.dev@gmail.com>
2025-08-15 01:57:29 +0000

    Remove old backend and frontend from root after restructuring

--- backend/authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/',views.LoginView.as_view(), name='login_page'),
-    path('signup/',views.SignupView.as_view(), name='signup_page'),
-    path('logout/',views.LogoutView.as_view(), name='logout_page'),
-
-]
--- backend/core/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-
-class Landing_Home(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, 'landing_home.html')
